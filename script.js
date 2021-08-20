d5ac93dfce82c824be0172693af6507554c18139
Ninua-Jin <ni.jintcharadze@gmail.com>
2021-08-20 16:14:33 +0400

    alert add

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ let burger = document.getElementById('bar');
 burger.addEventListener('click', () => {
     navbar.classList.toggle('active');
 })
+
+// click event
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+    swal({
+        title: "Coming soon!",
+        text: "მადლობა დაინტერესებისთვის! კითხვის გაგზავნის ფუნქცია მალე დაემატება საიტს, შეგიძლიათ დაგვიკავშირდეთ სოციალური ქსელების დახმარებით.",
+        icon: "info",
+      });
+});
