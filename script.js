858a8fa055e97439d9fde47d2d80ddb74a32a501
Ninua-Jin <ni.jintcharadze@gmail.com>
2021-08-20 19:55:28 +0400

    cards done

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ burger.addEventListener('click', () => {
 })
 
 // click event
-const button = document.querySelector('button');
+const button = document.querySelector('.btn');
 
 button.addEventListener('click', event => {
     swal({
         title: "Coming soon!",
         text: "მადლობა დაინტერესებისთვის! კითხვის გაგზავნის ფუნქცია მალე დაემატება საიტს, შეგიძლიათ დაგვიკავშირდეთ სოციალური ქსელების დახმარებით.",
-        icon: "info",
+        icon: "error",
       });
 });
